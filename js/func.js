51a86d6481e23b9ba3669ba2f7742a376f7f1039
aliyun4459011687 <dhrhero@qq.com>
2022-08-08 23:33:04 +0800

    优化

--- js/func.js
+++ js/func.js
@@ -43,8 +43,7 @@ function showSuggestions(arr) {
         selectStatus = false
         span.classList.length && span.classList.remove('tip-border')
     }
-    console.log('222',body.clientHeight*0.9, arr.length*24,);
-    if(body.clientHeight*0.9>=arr.length*24){}
+    if (body.clientHeight * 0.9 >= arr.length * 24) { }
 }
 
 /**
@@ -72,13 +71,27 @@ function fileImport() {
  * 跳转页面
 */
 function jump(val) {
-    console.log('ppp',val);
+    console.log('ppp1', val);
     let targetLink = ''
+    // 判断内容是否是合格网页链接
+    if (val.slice(0, 4) === 'http') {
+        console.log('ppp2', val);
+        const reg = /^(((ht|f)tps?):\/\/)?([^!@#$%^&*?.\s-]([^!@#$%^&*?.\s]{0,63}[^!@#$%^&*?.\s])?\.)+[a-z]{2,6}\/?/
+        if (reg.test(val)) {
+            console.log('ppp3', val);
+            box.value = '';
+            tip.innerHTML = '';
+            window.open(val)
+            return
+        }
+    }
     // 回车时判断当前是否采取建议搜索
     if (selectStatus) {
+        console.log('ppp4', val);
         const el2 = tip.getElementsByClassName('tip-text-hover')
         if (!el2.length) return
-        val= el2[0].innerHTML;
+        console.log('ppp5', val);
+        val = el2[0].innerHTML;
         targetLink = command[val] ? command[val] : `${path[engine]}${val}`
         box.value = '';
         tip.innerHTML = '';
@@ -87,19 +100,11 @@ function jump(val) {
     }
     // 以_开头的
     if (/^#.*/.test(val)) {
+        console.log('ppp6', val);
         setting(val)
         return
     }
-    // 判断内容是否是合格网页链接
-    if (val.slice(0, 4) === 'http') {
-        const reg = /^(((ht|f)tps?):\/\/)?([^!@#$%^&*?.\s-]([^!@#$%^&*?.\s]{0,63}[^!@#$%^&*?.\s])?\.)+[a-z]{2,6}\/?/
-        if (reg.test(val)) {
-            box.value = '';
-            tip.innerHTML = '';
-            window.open(val)
-            return
-        }
-    }
+    console.log('ppp7', val);
     // 正常跳转
     targetLink = command[val] ? command[val] : `${path[engine]}${val}`
     box.value = '';
