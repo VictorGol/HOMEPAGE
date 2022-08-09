3b844598fae2644fe584ce97edc7cdaf0e36df74
aliyun4459011687 <dhrhero@qq.com>
2022-08-10 07:59:46 +0800

    修改云效的链接

--- js/global.js
+++ js/global.js
@@ -97,7 +97,7 @@ const command = {
     '微信公众号': 'https://mp.weixin.qq.com',
     '微信公众平台': 'https://mp.weixin.qq.com',
     '域名备案查询': 'https://beian.miit.gov.cn/#/Integrated/index',
-    '云效': 'https://account.aliyun.com/',
+    '云效': 'https://devops.aliyun.com/',
     '正则': 'https://www.runoob.com/jsref/jsref-obj-regexp.html',
 }
 
